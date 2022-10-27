aee6dd3995991571788e73cdfc8b551f975dc728
Phan Quang Minh Long <lordaizensousuke2001@gmail.com>
2022-10-27 22:16:59 +0700

    code

---
+++ trogiang_python_b1.py
@@ -0,0 +1,9 @@
+# chua co gi
+plt.rcParams["figure.figsize"] = [8, 5]
+plt.rcParams["figure.autolayout"] = True
+fig, ax = plt.subplots()
+
+df_chart['gross income'].plot(kind='bar', color='#DF9496')
+df_chart['Rating'].plot(secondary_y = True, marker='*', color='#727B84')
+
+plt.show()
